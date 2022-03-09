a7d89f826c40116ef53eebfdc2c913d16fab2f6c
Leandro8279 <Leandro8279@gmail.com>
2022-03-09 00:08:06 -0300

    MovieCard

--- dsmovie/frontend/src/pages/Listing/index.tsx
+++ dsmovie/frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,11 @@
+import { MovieCard } from "components/MovieCard";
 import { Pagination } from "components/Pagination";
 import "./styles.css";
 export function Listing() {
-  return <Pagination />;
+  return (
+    <>
+      <Pagination />
+      <MovieCard />
+    </>
+  );
 }
